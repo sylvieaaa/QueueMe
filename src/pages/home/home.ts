3e12123975bf36cc2b4b52b440e12e95178e0397
Soong Yi Xuan <yxsoong@gmail.com>
2018-04-12 17:34:25 +0800

    home  edited

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -27,9 +27,7 @@ export class HomePage {
 
 
   ionViewDidLoad() {
-    if (sessionStorage.getItem('customerEntity') != null) {
-      this.navCtrl.setRoot(MainPage);
-    }
+    
   }
 
   openForgetPasswordPage() {
