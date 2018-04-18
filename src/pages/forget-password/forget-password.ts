fba26db08f4d9dfb89e1e4f4b27ab986280c1bd0
Soong Yi Xuan <yxsoong@gmail.com>
2018-04-18 21:06:56 +0800

    change password

--- src/pages/forget-password/forget-password.ts
+++ src/pages/forget-password/forget-password.ts
@@ -1,5 +1,7 @@
+import { HomePage } from './../home/home';
+import { CustomerEntityProvider } from './../../providers/customer-entity/customer-entity';
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, ToastController } from 'ionic-angular';
 
 /**
  * Generated class for the ForgetPasswordPage page.
@@ -16,7 +18,7 @@ export class ForgetPasswordPage {
 
   username: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,public customerEntityProvider: CustomerEntityProvider, private toastCtrl: ToastController) {
   }
 
   ionViewDidLoad() {
@@ -24,7 +26,21 @@ export class ForgetPasswordPage {
   }
 
   resetPassword() {
-    
+    let toast = this.toastCtrl.create({
+      duration: 3000,
+      position: 'bottom'
+    });
+    this.customerEntityProvider.doResetPassword(this.username).subscribe(
+      response => {
+        toast.setMessage("Password has been reset. Please check your email for the new password");
+        toast.present();
+        this.navCtrl.setRoot(HomePage);
+      },
+      error => {
+        toast.setMessage("Unable to reset password");
+        toast.present();
+      }
+    );
   }
 
 }
