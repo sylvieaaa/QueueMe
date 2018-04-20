3dde688a2e0760ebd962840d2f5c74f9614746d8
Soong Yi Xuan <yxsoong@gmail.com>
2018-04-21 02:36:39 +0800

    forgot to save

--- src/pages/foodcourt/foodcourt.ts
+++ src/pages/foodcourt/foodcourt.ts
@@ -63,17 +63,10 @@ export class FoodcourtPage {
     myModal.onDidDismiss(vendorRating =>{
       console.log(vendorRating);
       vendor.rating= vendorRating;
-<<<<<<< HEAD
       console.log("-----");
       console.log(vendor.rating);
       console.log("******")
       //this.ionViewDidLoad();
-=======
-      // console.log("-----");
-      // console.log(vendor.rating);
-      // console.log("******")
-      this.ionViewDidLoad();
->>>>>>> d68e0ad33c822345cdfb3265569baab98d2c4b02
 
     });
     myModal.present();
